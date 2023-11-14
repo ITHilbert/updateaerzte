915ff017e6778602affce112263256696f8b1f94
webulous <info@webulous.de>
2023-11-14 09:37:10 +0100

    fix: Upload von PraxisTableSeeder, PraxisAerzteTableSeeder und DatabaseSeeder

--- src/UploadAerzteCommand.php
+++ src/UploadAerzteCommand.php
@@ -33,6 +33,34 @@ class UploadAerzteCommand extends Command
             'root' => base_path('/database/seeders'),
         ]);
 
+        if ($disk->exists('PraxisTableSeeder.php')) {
+            $praxisSeeder = $disk->get('PraxisTableSeeder.php');
+
+            if (! Storage::disk('sftp')->put('PraxisTableSeeder.php', $praxisSeeder)) {
+                $this->error('Write operation failed! (PraxisTableSeeder.php)');
+                return -1;
+            }
+
+            $this->info('Write operation successful! (PraxisTableSeeder.php)');
+        } else {
+            $this->error('No such file: PraxisTableSeeder.php.');
+            return -1;
+        }
+
+        if ($disk->exists('PraxisAerzteTableSeeder.php')) {
+            $praxisAerzteSeeder = $disk->get('PraxisAerzteTableSeeder.php');
+
+            if (! Storage::disk('sftp')->put('PraxisAerzteTableSeeder.php', $praxisAerzteSeeder)) {
+                $this->error('Write operation failed! (PraxisAerzteTableSeeder.php)');
+                return -1;
+            }
+
+            $this->info('Write operation successful! (PraxisAerzteTableSeeder.php)');
+        } else {
+            $this->error('No such file: PraxisAerzteTableSeeder.php.');
+            return -1;
+        }
+
         if ($disk->exists('DatabaseSeeder.php')) {
             $praxisSeeder = $disk->get('DatabaseSeeder.php');
 
